f740b62f8e071d1f425d2514299ba99d9c66d7da
cc <a1144630459@163.com>
2025-06-24 20:12:20 +0800

    新上传

--- app/src/main.c
+++ app/src/main.c
@@ -7,15 +7,11 @@
 
 LOG_MODULE_REGISTER(blinky, LOG_LEVEL_INF);
 
-#define SLEEP_TIME_MS 500
-
 #define LED0_NODE DT_ALIAS(led0)
-
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
 int main(void)
 {
-	int ret;
 	bool led_state = true;
 
 	if (!gpio_is_ready_dt(&led)) {
@@ -23,21 +19,11 @@ int main(void)
 		return 0;
 	}
 
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-	if (ret < 0) {
-		LOG_ERR("Failed to configure LED GPIO pin");
-		return 0;
-	}
-
+	gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
 	while (1) {
-		ret = gpio_pin_toggle_dt(&led);
-		if (ret < 0) {
-			LOG_ERR("Failed to toggle LED");
-			return 0;
-		}
-
+		gpio_pin_toggle_dt(&led);
 		LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
 		led_state = !led_state;
-		k_msleep(SLEEP_TIME_MS);
+		k_msleep(500);
 	}
 }
