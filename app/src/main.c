124770d600b4a4baef5c72389b33eed34407fb1a
cc <a1144630459@163.com>
2025-07-25 16:37:43 +0800

    led闪烁

--- app/src/main.c
+++ app/src/main.c
@@ -5,8 +5,10 @@
 #include <zephyr/sys/printk.h>
 #include <zephyr/logging/log.h>
 
+// 注册一个日志模块
 LOG_MODULE_REGISTER(blinky, LOG_LEVEL_INF);
 
+// 获取led0 的设备节点信息
 #define LED0_NODE DT_ALIAS(led0)
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
@@ -19,16 +21,16 @@ int main(void)
 		return 0;
 	}
 
+	//配置引脚初始化状态
 	gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
 	while (1) {
 		gpio_pin_toggle_dt(&led);
 		LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
 		led_state = !led_state;
-		k_msleep(500);
+		k_msleep(500);// Zephyr 的内核延时函数，睡眠 500ms
 		
 	}
 }
-// build:west build -p always -b esp32s3_devkitm/esp32s3/procpu app
-// qemu_cortex_m3
-//D:\zephyr_SDK\zephyr-sdk-0.17.0\arm-zephyr-eabi\bin\arm-zephyr-eabi-gcc --version
-// gd32e103v_eval
+// build:   west build -p always -b esp32s3_devkitm/esp32s3/procpu app
+// west espressif monitor
+// west build -t menuconfig
