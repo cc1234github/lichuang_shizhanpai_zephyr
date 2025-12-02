88e0460d30edcd1a916ace75afba30345892839d
cc <a1144630459@163.com>
2025-12-02 23:18:49 +0800

    添加编译烧录说明

--- apps/lvgl_display/X_Track/DataProc/DP_IMU.cpp
+++
@@ -1,9 +0,0 @@
-#include "DataProc.h"
-
-DATA_PROC_INIT_DEF(IMU)
-{
-    HAL::IMU_SetCommitCallback([](void* info, void* userData){
-        Account* account = (Account*)userData;
-        return account->Commit(info, sizeof(HAL::IMU_Info_t));
-    }, account);
-}
--- apps/lvgl_display/X_Track/HAL/HAL.cpp
+++
@@ -1,9 +0,0 @@
-#include "HAL.h"
-
-void HAL::HAL_Init()
-{
-    HAL::Power_Init();
-    HAL::Encoder_Init();
-    HAL::Buzz_Init();
-    HAL::GPS_Init();
-}
